7d653474070905a3ff3f46a4deaec92b8a95b561
danielleleigh <danielle.leigh90@gmail.com>
2021-12-31 16:37:38 -0600

    add curly brackets to component in imports. cleared error. page now properly populating

--- argosi/src/components/Login/login.jsx
+++ argosi/src/components/Login/login.jsx
@@ -1,11 +1,11 @@
-import React, { useState, Component } from "react";
-import { Form, Button, Container, FormLabel } from "react-bootstrap";
-import axios from "axios";
-import "./login.css";
+import React from 'react'
+import axios from 'axios'
+import {useState} from 'react'
+import {Container, Form, Button, FormLabel} from 'react-bootstrap'
 
-const Login = (props) => {
-  const [userName, setUserName] = useState("");
-  const [userPassword, setUserPassword] = useState("");
+const Login = () => {
+  const [userName, setUserName] = useState('');
+  const [userPassword, setUserPassword] = useState('');
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -13,58 +13,44 @@ const Login = (props) => {
   };
 
   const postUser = async (name, password) => {
-    let payload = { username: name, password: password };
-    let response = await axios.post(
-      `http://127.0.0.1:8000/api/auth/login/`,
-      payload
-    );
-    console.log(response.data);
-    localStorage.setItem("token", response.data.token);
-    window.location = "/";
+    let payload = {username: name, password:password};
+    let response = await axios.post(`http://localhost:3000/api/auth/login`, payload);
+      console.log(response.data);
+      localStorage.setItem("token", response.data.access);
+      window.location = "/"
   };
 
   return (
-    <div align="center" className="login-style">
-      <Container style={{ flex: 1, width: "40%", padding: "10%" }}>
-        <FormLabel
-          style={{
-            color: "white",
-            fontSize: "24px",
-            backgroundColor: "black",
-          }}
-        >
-          Login
+    <div>
+      <Container style={{ flex: 1, width: "50%", padding: "10px" }}>
+        <FormLabel>
+          <h1>Login</h1>
         </FormLabel>
-        <Form id="contact-form" onSubmit={onFormSubmit}>
-          <Form.Group controlId="formBasicFirstName">
+        <Form id="login-form" onSubmit={onFormSubmit}>
+          <Form.Group controlId="formBasicUsername">
             <Form.Control
               type="text"
               name="userName"
-              placeholder="User Name"
+              placeholder="userName"
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
             />
           </Form.Group>
           &nbsp;
-          <Form.Group controlId="formBasicLastName">
+          <Form.Group controlId="formBasicPassword">
             <Form.Control
               type="text"
               name="userPassword"
-              placeholder="Password"
+              placeholder="password"
               value={userPassword}
               onChange={(e) => setUserPassword(e.target.value)}
             />
-            &nbsp;
           </Form.Group>
-          &nbsp;
-          <Button type="submit" variant="dark">
-            Submit
-          </Button>
+          <Button type="submit">Submit</Button>
         </Form>
       </Container>
-      &nbsp;
     </div>
   );
-};
+}
 
 export default Login;
