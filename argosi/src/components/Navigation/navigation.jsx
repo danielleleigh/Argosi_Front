e0830e6406c00e36f77b3d62a2ee019ed6be2fbd
danielleleigh <danielle.leigh90@gmail.com>
2022-01-06 11:36:24 -0600

    update links

--- argosi/src/components/Navigation/navigation.jsx
+++ argosi/src/components/Navigation/navigation.jsx
@@ -21,7 +21,7 @@ const Navigation = ({ user }) => {
       >
         <Navbar.Brand style={{ paddingRight: "25%" }}>
           <img
-            src="https://i.ibb.co/b1b9kbp/Blank-500-x-500-2.png"
+            src="/logoargosi.png"
             alt="Blank-2000-x-2000-3"
             border="0"
             height="100"
@@ -33,14 +33,13 @@ const Navigation = ({ user }) => {
           <Nav.Link href="/">Home</Nav.Link>
           {!user && (
             <React.Fragment>
-              <Nav.Link href="/book">Book</Nav.Link>
               <Nav.Link href="/login">Login</Nav.Link>
             </React.Fragment>
           )}
           {user && (
             <React.Fragment>
-              <Nav.Link href="dashboard/">Dashboard</Nav.Link>
-              <Nav.Link href="clients/">Clients</Nav.Link>
+              <Nav.Link  href="/dashboard/">Dashboard</Nav.Link>
+              <Nav.Link  href="/clients/">Clients</Nav.Link>
               <NavDropdown title="Contact">
                 <NavDropdown.Item href="/email">Email Client</NavDropdown.Item>
                 <NavDropdown.Item href="https://argosi.substack.com/">
