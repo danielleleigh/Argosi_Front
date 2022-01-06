dd9365cf8128231dccb7045809f3289b9e5e4c89
danielleleigh <danielle.leigh90@gmail.com>
2022-01-05 21:26:10 -0600

    create footer that stays on all pages

---
+++ argosi/src/components/Footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { MDBFooter, MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
+
+export default function Footer() {
+  return (
+    <MDBFooter backgroundColor="light" className="text-center text-lg-left">
+      <MDBContainer className="p-4">
+        <MDBRow></MDBRow>
+      </MDBContainer>
+
+      <div className="text-center p-3" style={{ backgroundColor: "#9DA5CE", opacity: "", fontFamily: "MoonwildDecorative" }}>
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a className="text-dark">Danielle Allen</a>
+      </div>
+    </MDBFooter>
+  );
+}
