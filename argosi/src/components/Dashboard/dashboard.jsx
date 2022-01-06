9674878f6367b92735b5b085068efc22b62b24d3
danielleleigh <danielle.leigh90@gmail.com>
2022-01-05 21:36:44 -0600

    styling to dashboard. moved calendar, add title

--- argosi/src/components/Dashboard/dashboard.jsx
+++ argosi/src/components/Dashboard/dashboard.jsx
@@ -7,14 +7,31 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        <ReactEmbeddedGoogleCalendar
-          publicUrl="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FChicago&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=WEEK&src=YXJnb3NpMjAyMUBnbWFpbC5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
-          style="border-width:0"
-          width="800"
-          height="600"
-          frameborder="0"
-          scrolling="no"
-        />
+    
+        <h1
+          style={{
+            textAlign: "center",
+            textDecoration: "underline",
+            color: "#FFC4C1",
+            fontFamily: "MoonwildDecorative",
+            fontWeight: "bold",
+            paddingTop: "1%",
+            
+          }}
+        >
+          Calendar
+        </h1>
+
+        <div style={{ marginLeft: "20%", paddingTop: "1%" }}>
+          <ReactEmbeddedGoogleCalendar
+            publicUrl="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FChicago&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&mode=WEEK&src=YXJnb3NpMjAyMUBnbWFpbC5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
+            style="border-width:0"
+            width="800"
+            height="600"
+            frameborder="0"
+            scrolling="no"
+          />
+        </div>
       </div>
     );
   }
