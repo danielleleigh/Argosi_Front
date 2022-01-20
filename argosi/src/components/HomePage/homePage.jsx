e842a5e485171418c2781c005c57d7b253ef95d1
danielleleigh <danielle.leigh90@gmail.com>
2022-01-20 10:36:39 -0600

    new font

--- argosi/src/components/HomePage/homePage.jsx
+++ argosi/src/components/HomePage/homePage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { ReactEmbeddedGoogleCalendar } from "react-embedded-google-calendar";
 import SchedulerCalendar from "scheduler-calendar";
-import { Table, Card, Container, Row, Col } from "react-bootstrap";
+import { Table, Card, Container, Row, Col, Carousel } from "react-bootstrap";
 
 //landing page. allows customers to visit the app and make appointment 
 //or see articles. also available after login for user to book clients 
@@ -10,50 +10,100 @@ import { Table, Card, Container, Row, Col } from "react-bootstrap";
 class HomePage extends Component {
   render() {
     return (
-  <React.Fragment>
-   <Container>
-      <div id="carouselExampleDark" class="carousel carousel-light slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="/article1.png" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="/article2.png" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="/article3.png" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-      </Container>
-        )
+      <React.Fragment>
+        <Container>
+          <Carousel fade style={{ padding: "10%" }}>
+            <Carousel.Item>
+              <img className="d-block w-100" src="/article1.png" alt="" />
+              <Carousel.Caption>
+                <h3
+                  style={{
+                    color: "white",
+                    fontFamily: "Sunroll-Bold",
+                    textShadow: "50%",
+                    fontWeight: "bold",
+                  }}
+                >
+                  'Release These 4 Unhealthy Patterns Under The Pisces Full Moon
+                  To Achieve Your Heart’s Desire'
+                </h3>
+                <h4>
+                  <a
+                    style={{
+                      color: "#FFC4C1",
+                      fontFamily: "Blanka-Regular",
+                      letterSpacing: "15px",
+                    }}
+                    href="https://www.astrotaylormoon.com/post/release-these-4-unhealthy-patterns-under-the-pisces-full-moon-to-achieve-your-heart-s-desire"
+                  ></a>
+                </h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="/taylorarticle.jpeg"
+                alt="City Bureau"
+              />
+              <Carousel.Caption>
+                <h3
+                  style={{
+                    color: "#0E162C",
+                    fontFamily: "MoonwildDecorative",
+                    fontWeight: "bold",
+                  }}
+                >
+                  "Spiritual Healing at the Center of Self-Empowerment"
+                </h3>
+                <h4>
+                  <a
+                    style={{
+                      color: "#FFC4C1",
+                      fontFamily: "MoonwildDecorative",
+                      fontWeight: "bold",
+                    }}
+                    href="https://www.citybureau.org/newswire/2021/5/17/spiritual-healing-at-the-center-of-self-empowerment"
+                  >
+                    Read Article
+                  </a>
+                </h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="/yellowlady.jpg"
+                alt="Parade Article"
+              />
+              <Carousel.Caption>
+                <h3
+                  style={{
+                    color: "#0E162C",
+                    fontFamily: "MoonwildDecorative",
+                    fontWeight: "bold",
+                  }}
+                >
+                  "Good Morning, Mercury Retrograde!"
+                </h3>
+                <h4>
+                  {" "}
+                  <a
+                    style={{
+                      color: "#D4A58B",
+                      fontFamily: "MoonwildDecorative",
+                      fontWeight: "bold",
+                    }}
+                    href="https://parade.com/1102906/taylormoon/what-is-mercury-retrograde/"
+                  >
+                    Read Article
+                  </a>
+                </h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Container>
       </React.Fragment>
-    )
+    );
   }  }
 
 export default HomePage;
